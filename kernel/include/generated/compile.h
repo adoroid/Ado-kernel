e63d5bc145e78db7988204dd37c1bcc586691352
adonos <adonos@adonos-NHAB.(none)>
2013-03-31 23:37:48 +0900

    first commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 8 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#8 SMP PREEMPT Sat Mar 30 14:08:05 JST 2013"
+#define LINUX_COMPILE_BY "adonos"
+#define LINUX_COMPILE_HOST "adonos-NHAB"
+#define LINUX_COMPILER "gcc version 4.7.3 20130205 (prerelease) (Linaro GCC 4.7-2013.02-01) "
