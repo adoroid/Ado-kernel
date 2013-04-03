71661330519c68d6fa263b4367f1c60f83087fee
adonos <adonos@adonos-NHAB.(none)>
2013-04-03 23:21:42 +0900

    add governors and I/O schedulers

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 14 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Sat Mar 30 14:08:05 JST 2013"
+#define UTS_VERSION "#14 SMP PREEMPT Wed Apr 3 21:37:30 JST 2013"
 #define LINUX_COMPILE_BY "adonos"
 #define LINUX_COMPILE_HOST "adonos-NHAB"
 #define LINUX_COMPILER "gcc version 4.7.3 20130205 (prerelease) (Linaro GCC 4.7-2013.02-01) "
